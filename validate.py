1310fd54f8eb75b672c6caeed7a8e23661861c10
sagar1024 <141512763+sagar1024@users.noreply.github.com>
2023-08-19 20:21:17 +0530

    Add files via upload

---
+++ validate.py
@@ -0,0 +1,10 @@
+import xmlschema
+xml_file = "WSDLabex4.xml"
+xsd_file = "WSDLabex4.xsd"
+
+validator = xmlschema.XMLSchema(xsd_file)
+if validator.is_valid(xml_file):
+    print("XML file is valid against the XSD schema.")
+else:
+    print("XML file is not valid against the XSD schema.")
+    print(validator.validate(xml_file))
